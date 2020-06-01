a882dfaedc6714d0abbd14b25d7372f003d3d7e1
Serhiy <syosypenko@gmail.com>
2020-06-02 01:56:38 +0300

    no need hook

--- lib/easy_login/hooks.rb
+++ lib/easy_login/hooks.rb
@@ -1,11 +1,5 @@
 module RedmineOmniauth2Google
   class Hooks < Redmine::Hook::ViewListener
-    def view_account_login_bottom(context = {})
-      context[:controller].send(:render_to_string, {
-        :partial => "hooks/view_account_login_bottom",
-        :locals => context})
-    end
-
     def view_layouts_base_body_bottom(context = {})
       context[:controller].send(:render_to_string, {
         :partial => "hooks/view_users",
